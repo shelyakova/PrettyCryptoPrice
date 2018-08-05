711e1ecf997e9a0b0617d63b4f6b5b663e9d82d7
shelyakova <s.sheliakova@gmail.com>
2018-08-06 01:41:47 +0300

    Styled app in my comfortable,old way

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
+
 import axios from "axios";
 import Select from 'react-select';
+import CryptoCurrency from './CryptoCurrency.js';
+import './App.css';
 
 const options = [
   { value: 'USD', label: 'USD', icon: '$' },
@@ -9,60 +12,74 @@ const options = [
   { value: 'GBP', label: 'GBP', icon: '₴' }
 ];
 
+
 class App extends Component {
   state = {
-    currency: "EUR",
-    cryptos: [],
-    selectedOption: { value: 'GBP', label: 'GBP', icon: '₴' }
+    ETH: [],
+    LTC: [],
+    BTC: [],
+    selectedOption: { value: 'GBP', label: 'GBP', icon: '₴' },
+    cryptoCurrencies: {
+      Ethereum: 'ETH',
+      Litecoin: 'LTC',
+      Bitcoin: 'BTC'
+    }
   };
 
-  getData(currency) {
-    axios.get('https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC'+currency)
+  getData(crypto, currency) {
+    axios.get('https://apiv2.bitcoinaverage.com/indices/global/ticker/'
+    + crypto
+    + currency)
     .then(res => {
       const cryptos = res.data;
-      this.setState({cryptos: cryptos});
-      console.log(cryptos.changes.price.day);
+      crypto === 'BTC' && this.setState({BTC: cryptos});
+      crypto === 'LTC' && this.setState({LTC: cryptos});
+      crypto === 'ETH' && this.setState({ETH: cryptos});
     })
   }
 
-
   render() {
-    const { selectedOption, cryptos } = this.state;
+    const { selectedOption } = this.state;
       return (
               <div>
-                <Select
-                  value={selectedOption}
-                  onChange={(selectedOption) => {
-                    this.setState({ selectedOption });
-                    this.getData(selectedOption.value);
-                  }}
-                  options={options}
-                />
-                <div>
-                  <b>BTC</b>
-                  <br />
-
-                  <label>Current price:</label>
-                  {cryptos.ask}
-                  {cryptos.ask && selectedOption.icon}
-                  <br />
-
-                  <label>Hour change:</label>
-                  {cryptos.ask && cryptos.changes.price.hour}
-                  <br />
-
-                  <label>Day change:</label>
-                  {cryptos.ask && cryptos.changes.price.day}
-                  <br />
-
-                  <label>Week change:</label>
-                  {cryptos.ask && cryptos.changes.price.week}
-                  <br />
-
-                  <label>Month change:</label>
-                  {cryptos.ask && cryptos.changes.price.month}
-                  <br />
-
+                <div className="head">
+                  <div className="headText">
+                    Select currency to exchange:
+                  </div>
+                  <Select
+                    className="selectCurrency"
+                    value={selectedOption}
+                    onChange={(selectedOption) => {
+                      this.setState({ selectedOption });
+                      Object.values(this.state.cryptoCurrencies).map((item, key) => {
+                        return this.getData(item, selectedOption.value);
+                      });
+                    }}
+                    options={options}
+                  />
+                </div>
+                <div className="crytosPosition">
+                  <div className='Ethereum'>
+                    <CryptoCurrency
+                      name='Ethereum'
+                      crypto={this.state.ETH}
+                      icon={this.state.selectedOption.icon}
+                    />
+                  </div>
+                  <div className="Litecoin">
+                    <CryptoCurrency
+                      name='Litecoin'
+                      crypto={this.state.LTC}
+                      icon={this.state.selectedOption.icon}
+                    />
+                  </div>
+                  <div className="Bitcoin">
+                    <CryptoCurrency
+                      name='Bitcoin'
+                      crypto={this.state.BTC}
+                      icon={this.state.selectedOption.icon}
+                    />
+                  </div>
                 </div>
               </div>
             );
---
+++ src/CryptoCurrency.js
@@ -0,0 +1,132 @@
+import React, { Component } from "react";
+import Switch from "react-switch";
+import './App.css';
+import icon from './images/icon.png';
+
+
+class CryptoCurrency extends Component {
+  state = {
+    checked: false
+  };
+
+  render() {
+    const { crypto } = this.props;
+      return (
+              <div>
+                <div>
+                  <div>
+                  <label>{this.props.name}</label>
+                  <div className="image">
+                  </div>
+                  <div className="price">
+                    <b>
+                      <div className="priceText">
+                        <label>Current price:</label>
+                      </div>
+                      <div className="priceNumber">
+                        {crypto.ask}
+                        {crypto.ask && this.props.icon}
+                      </div>
+                    </b>
+                  </div>
+
+                  <div className="switch">
+                    <div className="switchName">
+                      <label>
+                        {this.state.checked === false ?
+                          'Percent change' : 'Price change'}
+                      </label>
+                    </div>
+                    <div className="switchButton">
+                      <label htmlFor="normal-switch">
+                        <Switch
+                          onChange={() => {
+                            this.state.checked === false ?
+                            this.setState({checked: true}) :
+                            this.setState({checked: false});
+                          }}
+                          checked={this.state.checked}
+                          onColor="#3eb3ef"
+                          handleDiameter={20}
+                          uncheckedIcon={false}
+                          checkedIcon={false}
+                          offColor="#bebebe"
+                          onColor="#3db0ea"
+                          boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+                          activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+                          height={15}
+                          width={40}
+                          className="react-switch"
+                          id="normal-switch"
+                        />
+                      </label>
+                    </div>
+                  </div>
+
+                  <table>
+                    <tr>
+                      <td className="textName">
+                        <label>Hour change:</label>
+                      </td>
+                      <td className={
+                        crypto.ask && crypto.changes.percent.hour < 0 ?
+                        'plusPrice' : 'minusPrice'
+                      }>
+                        {crypto.ask && this.state.checked === false &&
+                           crypto.changes.percent.hour+'%'}
+                        {crypto.ask && this.state.checked === true &&
+                            crypto.changes.price.hour+this.props.icon}
+                      </td>
+                    </tr>
+                    <tr>
+                      <td className="textName">
+                        <label>Day change:</label>
+                      </td>
+                      <td className={
+                        crypto.ask && crypto.changes.percent.day < 0 ?
+                        'plusPrice' : 'minusPrice'
+                      }>
+                        {crypto.ask && this.state.checked === false &&
+                         crypto.changes.percent.day+'%'}
+                        {crypto.ask && this.state.checked === true &&
+                          crypto.changes.price.day+this.props.icon}
+                      </td>
+                    </tr>
+                    <tr>
+                      <td className="textName">
+                        <label>Week change:</label>
+                      </td>
+                      <td className={
+                        crypto.ask && crypto.changes.percent.week < 0 ?
+                        'plusPrice' : 'minusPrice'
+                      }>
+                        {crypto.ask && this.state.checked === false &&
+                         crypto.changes.percent.week+'%'}
+                        {crypto.ask && this.state.checked === true &&
+                          crypto.changes.price.week+this.props.icon}
+                      </td>
+                    </tr>
+                    <tr>
+                      <td className="textName">
+                        <label>Month change:</label>
+                      </td>
+                      <td className={
+                        crypto.ask && crypto.changes.percent.month < 0 ?
+                        'plusPrice' : 'minusPrice'
+                      }>
+                        {crypto.ask && this.state.checked === false &&
+                           crypto.changes.percent.month+'%'}
+                        {crypto.ask && this.state.checked === true &&
+                            crypto.changes.price.month+this.props.icon}
+                      </td>
+                    </tr>
+                  </table>
+                </div>
+              </div>
+              </div>
+            );
+          }
+        }
+
+
+export default CryptoCurrency;
